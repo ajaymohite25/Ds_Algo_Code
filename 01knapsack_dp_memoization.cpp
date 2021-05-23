117cd73ad6c1bd8f51b49b726d8a13e30e939f39
Ajay mohite <ajaymohite244@gmail.com>
2021-05-23 16:58:56 +0530

    added new line

--- 01knapsack_dp_memoization.cpp
+++ 01knapsack_dp_memoization.cpp
@@ -2,7 +2,7 @@
 #include <cstring>
 using namespace std;
 int t[1002][1002];
-int count=0;
+int count = 0;
 int knapSack(int W, int wt[], int val[], int n)
 {
     count++;
@@ -14,7 +14,7 @@ int knapSack(int W, int wt[], int val[], int n)
 
     if (wt[n - 1] <= W)
     {
-        return t[W][n] = max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n-1 ), knapSack(W, wt, val, n - 1));
+        return t[W][n] = max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1));
     }
     else
     {
@@ -27,10 +27,10 @@ int main()
     for (int i = 0; i < 1002; i++)
         for (int j = 0; j < 1002; j++)
             t[i][j] = -1;
-    int values[] = {1, 2,1 };
-    int weight[] = {4, 5, 9 };
+    int values[] = {1, 2, 1};
+    int weight[] = {4, 5, 9};
     int n = 3, w = 9;
-    cout << knapSack(w, weight, values, n)<<endl;
-    cout<<count;
+    cout << knapSack(w, weight, values, n) << endl;
+    cout << count << endl;
     return 0;
 }
